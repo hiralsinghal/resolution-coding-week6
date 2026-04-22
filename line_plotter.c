8b38a98d4c2e465815d8f6e0c33d551b47b6a317
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-22 05:06:56 +0000

    worked more cause ai classes boring

--- line_plotter.c
+++ line_plotter.c
@@ -4,6 +4,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#define SLEEP(t) Sleep(t * 1000)
+#define CLEAR_SCREEN() system("cls")
+#else
+#include <unistd.h>
+#define SLEEP(t) sleep(t)
+#define CLEAR_SCREEN() system("clear")
+#endif
+
 #define HEIGHT 30
 #define WIDTH 100
 
@@ -15,6 +25,36 @@ int main() {
 
     SLEEP(5);
     CLEAR_SCREEN();
+
+    return 0;
+}
+
+int make_screen_array(char *screen_buffer) {
+    screen_buffer[0] = '\0';
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            strcat(screen_buffer, " ");
+        }
+        if (y < HEIGHT - 1) {
+            strcat(screen_buffer, "\n")
+        }
+    }
+
+    return 0;
+}
+
+int draw_symbol(int ball_x, int ball_y, char *screen_buffer, char symbol) {
+    if (ball_x < 0 || ball_x >= WIDTH || ball_y < 0 || ball_y >= HEIGHT) {
+        return 1;
+    }
+
+    int location = ball_y * (WIDTH + 1) + ball_x;
+    screen_buffer[location] = symbol;
+
+    printf("\x1b[H");
+    printf("%s", screen_buffer);
+
+    fflush(stdout);
 
     return 0;
 }
